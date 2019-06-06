4f297ece39f6fc95ba6a836f5ae74eeacb188971
DanielFlores23 <49959778+DanielFlores23@users.noreply.github.com>
2019-06-05 21:01:58 -0600

    Add files via upload

---
+++ mi_comida_favorita_.py
@@ -0,0 +1,40 @@
+comidas = ["Chicken soup = Sopa de pollo."
+    "Rice & beans = Arroz y frijoles."
+    "Mondongo soup = Sopa de mondongo."
+    "Beef soup = Sopa de res."
+    "Seafood = Sopa marinera."
+    "Spaghetti = Espagueti."
+    "Fried chicken = Pollo frito."
+	"Grilled chicken = Pollo asado"
+	"Baleadas"
+	"Tortillas con quesillo"
+	"Caracol a la plancha"
+	"Pescado"
+	"Emparedado"
+	"Chorizo a la parilla"
+	"Chuleta en salsa de hongo"
+	"Papas fritas" 
+	"Salmon en salsa de hongo" 
+	"Albondigas"
+	"Pollo a la naranja"
+	"Pinchos de chorizo"]
+
+def imprimir menu()
+	print("escoge tus 5 comidas favoritas :")
+	for comida in comidas 
+		print(comida)
+		
+imprimir_menu()		
+
+print()
+comidas favoritas = ["Rice & beans = Arroz y frijoles."
+    "Seafood = Sopa marinera."
+    "Pescado", "Emparedado"
+	"Tortillas con quesillo"]
+	
+	def imprimir menu()
+	print("estan son mis 5 comidas favoritas :")
+	for comida in comidas 
+		print(comida)
+		
+imprimir_menu()	
